e065f611303fbe74762faaaa1f8aa5339b2f4e8b
CHIRANJEEVI <chiranjeevi2004@gmail.com>
2023-09-03 10:51:43 +0530

    status : cart

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -10,7 +10,7 @@ const Products = () =>{
             <img src="./product1.jpg" className="rounded-lg grayscale hover:grayscale-[calc(0%)] duration-300"/>
             <div className="absolute w-full flex flex-col items-center text-5xl text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 uppercase">
                 <div className="px-6 py-6">
-                    <h1 className="text-center lg:text-start text-4xl lg:text-2xl">tShirts</h1>
+                    <h1 className="text-start text-4xl lg:text-2xl">tShirts</h1>
                     <p className="text-xs">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
                 </div>
             </div>
