0693a63fb94adc88dacbaed75ae22d93b4106227
CHIRANJEEVI <chiranjeevi2004@gmail.com>
2023-09-04 19:52:11 +0530

    status : props

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -9,7 +9,7 @@ const Products = ({imgUrl,category}) =>{
         <div className="mt-5 lg:mt-0 relative w-[calc(100%)] lg:w-[calc(30%)] lg:hover:scale-110 duration-300">
             <img src={imgUrl} className="rounded-lg grayscale hover:grayscale-[calc(0%)] duration-300"/>
             <div className="absolute w-full bottom-0 text-5xl text-white uppercase">
-                <div className="px-6 py-2 bg-black text-white">
+                <div className="px-6 py-2 bg-black text-white rounded-b-lg">
                     <h1 className="text-start text-2xl lg:text-2xl">{category}</h1>
                 </div>
             </div>
